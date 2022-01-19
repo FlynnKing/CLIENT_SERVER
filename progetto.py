650e21ce855cde363a1d17dffbbab7592068a371
Davide <davide97@gmail.com>
2022-01-19 10:30:57 +0100

    secondo commit

--- progetto.py
+++ progetto.py
@@ -7,23 +7,17 @@ class Item:
     price = 0
     quantity = 0
     def __init__(self, name:str, price:float, quantity=0):
-        self.price = 2374
-        self.quantity = 10
-        if quantity>0 or price>0:
-            print("la quantità e il prezzo sono maggiori di zero!!")
-        
-    def calculate_total_price(x, y):
-        return x * y
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        assert price >=0, f"il prezzo {price} è minore di zero!"
+        assert quantity >=0, f"la quantità {price} è minore di zero!"
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
-item1 = Item
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+item1 = Item("Phone", 100, 5)
+item2 = Item("laptop", 1000, 3)
 print("il totale del Phone prezzo è: ")
-print(item1.calculate_total_price(item1.price, item1.quantity))
-item2 = Item
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
+print(item1.calculate_total_price())
 print("il totale del Laptop prezzo è: ")
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print(item2.calculate_total_price())
