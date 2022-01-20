5eb5abc5b605d737a0aa59bcb32d90de99683caa
Davide <davide97@gmail.com>
2022-01-20 11:35:38 +0100

    terzo commit

--- progetto.py
+++ progetto.py
@@ -3,17 +3,23 @@ from email import message
 
 
 class Item:
-    name= ""
-    price = 0
-    quantity = 0
-    def __init__(self, name:str, price:float, quantity=0):
+    pay_rate = 0
+    all_items = [] #lista delle istanze
+    def __init__(self, name:str, price:float, quantity=0, ):
         self.name = name
         self.price = price
         self.quantity = quantity
+        self.all_items.append()
         assert price >=0, f"il prezzo {price} è minore di zero!"
         assert quantity >=0, f"la quantità {price} è minore di zero!"
     def calculate_total_price(self):
         return self.price * self.quantity
+    def apply_discount(self):
+        self.pay_rate = input("inserisci lo sconto tra 0 e 1(float): ") 
+
+        return self.price 
+    def __repr__(self) -> str:
+        pass
 
 item1 = Item("Phone", 100, 5)
 item2 = Item("laptop", 1000, 3)
@@ -21,3 +27,5 @@ print("il totale del Phone prezzo è: ")
 print(item1.calculate_total_price())
 print("il totale del Laptop prezzo è: ")
 print(item2.calculate_total_price())
+
++
